08202931e5425d3473c53ac0c37e7af766643aa2
ScreamingGrandma <Keanu.Ngov@Gmail.com>
2025-08-02 14:14:55 -0700

    Code: destroy animal with food

---
+++ Assets/Scripts/DetectCollsion.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DetectCollsion : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void OnTriggerEnter(Collider other)
+    {
+        Destroy(other.gameObject);
+        Destroy(gameObject);
+    }
+}
--- Assets/Scripts/MoveForward.cs
+++ Assets/Scripts/MoveForward.cs
@@ -24,6 +24,7 @@ public class MoveForward : MonoBehaviour
         }
         else if (transform.position.z < bottomBoundary)
         {
+            Debug.Log("Game Over");
             Destroy(gameObject);
         }
     }
