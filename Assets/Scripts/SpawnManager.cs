c06a94c7b806d15242ce61e00d3dfbc5386eb72d
ScreamingGrandma <Keanu.Ngov@Gmail.com>
2025-08-02 13:47:11 -0700

    Coded: spawnmanager

--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -5,6 +5,9 @@ using UnityEngine;
 public class SpawnManager : MonoBehaviour
 {
     public GameObject[] animalPrefabs;
+    private int animalIndex;
+    public float spawnRangeX = 15.0f;
+    public float spawnPosZ = 30.0f;
     // Start is called before the first frame update
     void Start()
     {
@@ -14,6 +17,11 @@ public class SpawnManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            animalIndex = Random.Range(0, animalPrefabs.Length);
+            Instantiate(animalPrefabs[animalIndex], new Vector3(Random.Range(-spawnRangeX, spawnRangeX), 0, spawnPosZ), animalPrefabs[animalIndex].transform.rotation);
+        }
         
     }
 }
